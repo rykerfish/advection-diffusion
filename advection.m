d32247dd1ecafa867e39f0a27afe3db88b7d633c
Ryker Fish <rfish@mines.edu>
2023-04-18 15:13:54 -0600

    added code to do the laplacian part of the equation

--- advection.m
+++ advection.m
@@ -8,5 +8,68 @@ clf
 clearvars
 
 
-N = 64;
+%%% Setup grid in x-y
+N = 10; % number of grid point along one direction
+x = linspace(-5,5,N+1); % type 1 grid
+dx = x(2)-x(1);
+% convert to type 2 grid
+x = x(1:end-1) + dx/2.0;
 
+y = x;
+dy = dx;
+h = dx;
+
+
+[X,Y] = meshgrid(x,y); % make 2D grid
+
+% initial condition
+F = @(x, y) exp(-(x.^2+y.^2));
+u_0 = F(X, Y);
+
+% time stepping parameters
+CFL = 0.001;
+dt = CFL*(dx);
+T_final = 10;
+N_steps = T_final/dt;
+
+
+u = u_0;
+for n = 1:N_steps
+
+    u_lap = zeros(size(u));
+    % 2D finite laplacian
+    for x_i = 1:N
+        for y_i = 1:N
+
+            u_center = u(x_i, y_i);
+            if x_i == 1 % left edge
+                u_west = u_center;
+            else
+                u_west = u(x_i-1, y_i);
+            end
+            if x_i == N % right edge
+                u_east = u_center;
+            else
+                u_east=u(x_i+1, y_i);
+            end
+            if y_i == 1 % bottom edge
+                u_south = u_center;
+            else
+                u_south = u(x_i,y_i-1);
+            end
+            if y_i == N % top edge
+                u_north = u_center;
+            else
+                u_north=u(x_i, y_i+1);
+            end
+
+            % finite difference laplacian w 5 point stencil
+            u_lap(x_i, y_i) = (u_west + u_east + u_south + u_north - 4*u_center)*(1/h^2);
+
+        end
+    end
+
+    % still needs convection term added below
+    u = u + dt*D*u_lap;
+
+end
