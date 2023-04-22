7bf91cdda8b36f70ee7accc14730ddc091d6699f
Ryker Fish <rfish@mines.edu>
2023-04-22 10:03:02 -0600

    changed to periodic boundary conditions

--- advection.m
+++ advection.m
@@ -9,7 +9,7 @@ clearvars
 
 
 %%% Setup grid in x-y
-N = 100; % number of grid point along one direction
+N = 200; % number of grid point along one direction
 x = linspace(-3,3,N+1); % type 1 grid
 dx = x(2)-x(1);
 % convert to type 2 grid
@@ -23,8 +23,8 @@ h = dx;
 [X,Y] = meshgrid(x,y); % make 2D grid
 
 % initial condition
-F = @(x, y) 3*exp(-(x.^2+y.^2));
-u_0 = F(X, Y);
+% F = @(x, y) 3*exp(-(x.^2+y.^2));
+u_0 = ic(X, Y);
 
 % time stepping parameters
 % CFL = 0.001;
@@ -35,10 +35,11 @@ N_steps = T_final/dt;
 
 % physical parameters
 D = 0.01; % diffusion
+vel = @(x) 0.1;
 
 
 
-t_plot = 100;
+t_plot = 1000;
 
 u = u_0;
 for n = 1:N_steps
@@ -48,25 +49,25 @@ for n = 1:N_steps
     for x_i = 1:N
         for y_i = 1:N
 
-            % 2D finite laplacian
+            % 2D finite laplacian with periodic BC
             u_center = u(x_i, y_i);
             if x_i == 1 % left edge
-                u_west = u_center;
+                u_west = u(N, y_i);
             else
                 u_west = u(x_i-1, y_i);
             end
             if x_i == N % right edge
-                u_east = u_center;
+                u_east = u(1, y_i);
             else
                 u_east=u(x_i+1, y_i);
             end
             if y_i == 1 % bottom edge
-                u_south = u_center;
+                u_south = u(x_i, N);
             else
                 u_south = u(x_i,y_i-1);
             end
             if y_i == N % top edge
-                u_north = u_center;
+                u_north = u(x_i, 1);
             else
                 u_north=u(x_i, y_i+1);
             end
@@ -75,22 +76,29 @@ for n = 1:N_steps
             u_lap(x_i, y_i) = (u_west + u_east + u_south + u_north - 4*u_center)*(1/h^2);
 
 
-            if x_i == 1 || x_i == N
-                u_adv(x_i, y_i) = 0;
+            % periodic BC for advection
+            if x_i == 1
+                u_left = u(N, y_i);
             else
-                % upwinding
-                if(vel(x_i) > 0)
-                    u_adv(x_i, y_i) = vel(x_i)*(u(x_i, y_i) - u(x_i-1, y_i))/dx;
-                else
-                    u_adv(x_i, y_i) = vel(x_i)*(u(x_i+1, y_i) - u(x_i, y_i))/dx;
-                end
-            end 
+                u_left = u(x_i-1, y_i);
+            end
+            if x_i == N
+                u_right = u(1, y_i);
+            else
+                u_right = u(x_i+1, y_i);
+            end
 
-
-        end
+             % upwinding
+            if(vel(x_i) > 0)
+                u_adv(x_i, y_i) = vel(x_i)*(u(x_i, y_i) - u_left)/dx;
+            else
+                u_adv(x_i, y_i) = vel(x_i)*(u_right - u(x_i, y_i))/dx;
+            end
+                % centered difference
+%                 u_adv(x_i, y_i) = vel(x_i)*(u(x_i+1, y_i) - u(x_i-1,y_i))/(2*dx); % centered difference
+        end 
     end
 
-    % still needs advection term added below
     % forward euler
     u = u + dt*D*u_lap + u_adv*dt;
 
@@ -106,8 +114,22 @@ for n = 1:N_steps
 
 end
 
-function v_x = vel(x)
+% makes a cylinder as the initial condition
+function u_0 = ic(X, Y)
+
+    r = 1;
+    u_0 = zeros(size(X));
     
-    v_x = -0.05;
+    N = length(X);
+    for i = 1:N
+        for j = 1:N
+            if X(i, j)^2 + Y(i,j)^2 <= r
+                u_0(i, j) = 1;
+            else
+                u_0(i, j) = 0;
+            end
+        end
+    end
+
 
 end
