4008503dbf446156859dbbf6b3e47ed701c02f24
danielcrd <55002280+danielcrd@users.noreply.github.com>
2023-04-22 01:39:29 -0600

    comment main.c

--- serial_c_code/main.c
+++ serial_c_code/main.c
@@ -5,19 +5,30 @@
 
 int main() {
 
+	// declare vectors
 	Vector u_grid; 
 	Vector x_grid;
 	Vector y_grid;
 	
-	allocate_vector(&u_grid, 100, 100);
-	allocate_vector(&x_grid, 100, 100);
-	allocate_vector(&y_grid, 100, 100);
+	// allocate vectors
+	int rows = 100;
+	int cols = 100;
+	allocate_vector(&u_grid, rows, cols);
+	allocate_vector(&x_grid, rows, cols);
+	allocate_vector(&y_grid, rows, cols);
 	
-	create_grid(&x_grid, -3, 3, 'x');
-	create_grid(&y_grid, -3, 3, 'y');
+	// create grids for constructing the concentration grid
+	float x_start = -3.0;
+	float x_end = 3.0;
+	float y_start = -3.0;
+	float y_end = 3.0;
+	create_grid(&x_grid, x_start, x_end, 'x');
+	create_grid(&y_grid, y_start, y_end, 'y');
 
+	// construct the concentration grid
 	initialize_concentration_vector(&u_grid, &x_grid, &y_grid);
 	
+	// print grid to console for comparison
 	int i;
 	for(i = 0; i < 100; ++i){
 		int j;
@@ -26,7 +37,12 @@ int main() {
 		}
 		printf("\n");
 	}
-
+	
+	// clean up
+	deallocate_vector(&u_grid);
+	deallocate_vector(&x_grid);
+	deallocate_vector(&y_grid);
+	
 	return 0;
 
 }
